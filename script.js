af850725ede912d8fc32dee1c4280743094f4b67
Vasil Vanchuk <v.vanchuk@tut.by>
2016-01-17 20:02:55 +0300

    Add templates into directives

--- script.js
+++ script.js
@@ -2,16 +2,17 @@ angular.module('ngMail', [])
     .directive('ngmailSelectPageMenu', function() {
         return  {
             restrict: 'E',
+            scope: {
+              list: '='
+            },
             template: `
                     <div class="col-sm-3 col-md-2">
                         <div class="btn-group">
                             <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
-                                EMail <span class="caret"></span>
+                                {{list[0].title}} <span class="caret"></span>
                             </button>
                             <ul class="dropdown-menu" role="menu">
-                                <li><a href="#">Mail</a></li>
-                                <li><a href="#">Contacts</a></li>
-                                <li><a href="#">Tasks</a></li>
+                                <li ng-repeat="item in list"><a href="{{item.link}}">{{item.title}}</a></li>
                             </ul>
                         </div>
                     </div>`
@@ -20,14 +21,18 @@ angular.module('ngMail', [])
     .directive('ngmailListItem', function() {
         return {
             restrict: 'E',
+            replace: true,
+            scope: {
+                item: '='
+            },
             template: `
-                    <a href="#" class="list-group-item read">
+                    <a href="#" class="list-group-item {{item.wasRead? 'read': ''}}">
                         <div class="checkbox"><label><input type="checkbox"></label></div>
                         <span class="glyphicon glyphicon-star-empty"></span>
-                        <span class="name" style="min-width: 120px; display: inline-block;">Muhammad Faizan Uddin</span>
-                        <span class="">Nice work on the lastest version</span>
+                        <span class="name" style="min-width: 120px; display: inline-block;">{{item.title}}</span>
+                        <span class="">{{item.preview}}</span>
                         <span class="text-muted" style="font-size: 11px;">- More content here</span>
-                        <span class="badge">12:10 AM</span>
+                        <span class="badge">{{item.time}}</span>
                         <span class="pull-right"><span class="glyphicon glyphicon-paperclip"></span></span>
                     </a>`
         }
@@ -35,28 +40,87 @@ angular.module('ngMail', [])
     .directive('ngmailFoldersList', function() {
         return {
             restrict: 'E',
+            scope: {
+                list: '='
+            },
+            link: function(scope) {
+                scope.setActive = function(item) {
+                    this.list.forEach((item) => item.active = false);
+                    item.active = true;
+                };
+            },
             template: `
                 <ul class="nav nav-pills nav-stacked">
-                    <li class="active"><a href="#"><span class="badge pull-right">32</span> Inbox</a></li>
-                    <li><a href="#">Starred</a></li>
-                    <li><a href="#">Important</a></li>
-                    <li><a href="#">Sent Mail</a></li>
-                    <li><a href="#"><span class="badge pull-right">3</span>Drafts</a></li>
+                    <li ng-repeat="item in list" class="{{item.active? 'active': ''}}">
+                        <a href="{{item.link}}" ng-click="setActive(item);">
+                        <span class="badge pull-right" ng-show="item.unreadCount">{{item.unreadCount}}</span>
+                        {{item.title}}
+                        </a>
+                    </li>
                 </ul>`
         }
     })
     .directive('ngmailPageTabs', function() {
         return {
             restrict: 'E',
+            scope: {
+                list: '='
+            },
             template: `
                 <ul class="nav nav-tabs">
-                    <li class="active"><a href="#home" data-toggle="tab"><span class="glyphicon glyphicon-inbox"></span>Primary</a></li>
-                    <li><a href="#profile" data-toggle="tab"><span class="glyphicon glyphicon-user"></span>Social</a></li>
-                    <li><a href="#messages" data-toggle="tab"><span class="glyphicon glyphicon-tags"></span>Promotions</a></li>
-                    <li><a href="#settings" data-toggle="tab"><span class="glyphicon glyphicon-plus no-margin"></span></a></li>
+                    <li ng-repeat="item in list" class="{{item.active? 'active': ''}}"><a href="#{{item.id}}" data-toggle="tab"><span class="glyphicon {{item.icon}}"></span>{{item.title}}</a></li>
                 </ul>`
         }
     })
+    .directive('ngmailListPaginator', function() {
+        return {
+            restrict: 'E',
+            scope: {
+                state: '='
+            },
+            template: `
+                    <span class="text-muted"><b>{{state.from}}</b>–<b>{{state.to}}</b> of <b>{{state.count}}</b></span>
+                    <div class="btn-group btn-group-sm">
+                        <button type="button" class="btn btn-default">
+                            <span class="glyphicon glyphicon-chevron-left"></span>
+                        </button>
+                        <button type="button" class="btn btn-default">
+                            <span class="glyphicon glyphicon-chevron-right"></span>
+                        </button>
+                    </div>`
+        }
+    })
     .controller('PageController', function() {
+        this.pageMenuItems = [
+            {title: 'Email', link: '#email', active: true },
+            {title: 'Contacts', link: '#contact' },
+            {title: 'Another menu', link: '#another_menu' }
+        ];
 
+        this.mailListItems = [1, 2, 3, 4, 5, 6, 7].map((item) => ({
+            title: `Email title ${item}`,
+            preview: `Preview for email #${item}....`,
+            time: `${Math.floor(Math.random()*12)}:${Math.floor(Math.random()*60)} ${(Math.random()>0.5) ? 'AM' : 'PM'}`,
+            wasRead: Math.random() > 0.8
+        }));
+
+        this.mailFoldersList = [
+            {title: 'Inbox', link: '#inbox', active: true, unreadCount: Math.floor(Math.random()*100) },
+            {title: 'Starred', link: '#starred' },
+            {title: 'Important', link: '#important' },
+            {title: 'Sent Mail', link: '#send_mail' }
+        ];
+
+        this.paginationState = {
+            count: 150,
+            from: 20,
+            to: 40
+        };
+
+        this.pageTabs = [
+            {title: 'Primary', id: 'home', icon: 'glyphicon-inbox', active: true},
+            {title: 'Social', id: 'profile', icon: 'glyphicon-user'},
+            {title: 'Promotions', id: 'messages', icon: 'glyphicon-tags'},
+            {title: '', id: 'settings', icon: 'glyphicon-plus no-margin'}
+        ];
     });
