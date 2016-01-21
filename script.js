1e11d6b51786c1ae9f8822aa5d858ccdf46a286b
Vasil Vanchuk <v.vanchuk@tut.by>
2016-01-21 22:02:21 +0300

    Move data fetching into service with delay

--- script.js
+++ script.js
@@ -1,4 +1,42 @@
 angular.module('ngMail', [])
+    .service('delayedDataService', function($q, $timeout) {
+        return function(dataToResolve) {
+            var deferred = $q.defer();
+            var promise = deferred.promise;
+            $timeout(() => deferred.resolve(dataToResolve), Math.random() * 10000);
+            return promise;
+        }
+    })
+    .service('MailService', function(delayedDataService) {
+        this.getPageTabs = () => delayedDataService([
+                {title: 'Primary', id: 'home', icon: 'glyphicon-inbox', active: true},
+                {title: 'Social', id: 'profile', icon: 'glyphicon-user'},
+                {title: 'Promotions', id: 'messages', icon: 'glyphicon-tags'},
+                {title: '', id: 'settings', icon: 'glyphicon-plus no-margin'}
+            ]);
+        this.getPaginationState = () => delayedDataService({
+                count: 150,
+                from: 20,
+                to: 40
+            });
+        this.getMailFoldersList = () => delayedDataService([
+            {title: 'Inbox', link: '#inbox', active: true, unreadCount: Math.floor(Math.random()*100) },
+            {title: 'Starred', link: '#starred' },
+            {title: 'Important', link: '#important' },
+            {title: 'Sent Mail', link: '#send_mail' }
+        ]);
+        this.getMailListItems = () => delayedDataService([1,2,3,4,5,6,7,8,9,10,11].map((item) => ({
+            title: `Email title ${item}`,
+            preview: `Preview for email #${item}....`,
+            time: `${Math.floor(Math.random()*12)}:${Math.floor(Math.random()*60)} ${(Math.random()>0.5) ? 'AM' : 'PM'}`,
+            wasRead: Math.random() > 0.8
+        })));
+        this.getPageMenuItems = () => delayedDataService([
+            {title: 'Email', link: '#email', active: true },
+            {title: 'Contacts', link: '#contact' },
+            {title: 'Another menu', link: '#another_menu' }
+        ]);
+    })
     .directive('ngmailSelectPageMenu', function() {
         return  {
             restrict: 'E',
@@ -12,7 +50,7 @@ angular.module('ngMail', [])
                                 {{list[0].title}} <span class="caret"></span>
                             </button>
                             <ul class="dropdown-menu" role="menu">
-                                <li ng-repeat="item in list"><a href="{{item.link}}">{{item.title}}</a></li>
+                                <li ng-repeat="item in list track by $index"><a href="{{item.link}}">{{item.title}}</a></li>
                             </ul>
                         </div>
                     </div>`
@@ -51,7 +89,7 @@ angular.module('ngMail', [])
             },
             template: `
                 <ul class="nav nav-pills nav-stacked">
-                    <li ng-repeat="item in list" class="{{item.active? 'active': ''}}">
+                    <li ng-repeat="item in list track by $index" class="{{item.active? 'active': ''}}">
                         <a href="{{item.link}}" ng-click="setActive(item);">
                         <span class="badge pull-right" ng-show="item.unreadCount">{{item.unreadCount}}</span>
                         {{item.title}}
@@ -68,7 +106,7 @@ angular.module('ngMail', [])
             },
             template: `
                 <ul class="nav nav-tabs">
-                    <li ng-repeat="item in list" class="{{item.active? 'active': ''}}"><a href="#{{item.id}}" data-toggle="tab"><span class="glyphicon {{item.icon}}"></span>{{item.title}}</a></li>
+                    <li ng-repeat="item in list track by $index" class="{{item.active? 'active': ''}}"><a href="#{{item.id}}" data-toggle="tab"><span class="glyphicon {{item.icon}}"></span>{{item.title}}</a></li>
                 </ul>`
         }
     })
@@ -90,37 +128,10 @@ angular.module('ngMail', [])
                     </div>`
         }
     })
-    .controller('PageController', function() {
-        this.pageMenuItems = [
-            {title: 'Email', link: '#email', active: true },
-            {title: 'Contacts', link: '#contact' },
-            {title: 'Another menu', link: '#another_menu' }
-        ];
-
-        this.mailListItems = [1, 2, 3, 4, 5, 6, 7].map((item) => ({
-            title: `Email title ${item}`,
-            preview: `Preview for email #${item}....`,
-            time: `${Math.floor(Math.random()*12)}:${Math.floor(Math.random()*60)} ${(Math.random()>0.5) ? 'AM' : 'PM'}`,
-            wasRead: Math.random() > 0.8
-        }));
-
-        this.mailFoldersList = [
-            {title: 'Inbox', link: '#inbox', active: true, unreadCount: Math.floor(Math.random()*100) },
-            {title: 'Starred', link: '#starred' },
-            {title: 'Important', link: '#important' },
-            {title: 'Sent Mail', link: '#send_mail' }
-        ];
-
-        this.paginationState = {
-            count: 150,
-            from: 20,
-            to: 40
-        };
-
-        this.pageTabs = [
-            {title: 'Primary', id: 'home', icon: 'glyphicon-inbox', active: true},
-            {title: 'Social', id: 'profile', icon: 'glyphicon-user'},
-            {title: 'Promotions', id: 'messages', icon: 'glyphicon-tags'},
-            {title: '', id: 'settings', icon: 'glyphicon-plus no-margin'}
-        ];
+    .controller('PageController', function(MailService) {
+        MailService.getPageMenuItems().then((data) => this.pageMenuItems = data);
+        MailService.getMailListItems().then((data) => this.mailListItems = data);
+        MailService.getMailFoldersList().then((data) => this.mailFoldersList = data);
+        MailService.getPaginationState().then((data) => this.paginationState = data);
+        MailService.getPageTabs().then((tabs) => this.pageTabs = tabs);
     });
